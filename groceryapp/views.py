0d902b12ae64e12fd39c7388cbe6b41d6df1b6b6
HumairaIrin <humairamuntaha24@gmail.com>
2024-10-23 17:00:27 +0600

    all files

---
+++ groceryapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
